a6ba5ced767b18afe9d35e3dab68b990ce529f41
Luis Mantilla <luismantilla99@outlook.com>
2022-10-22 21:47:55 -0700

    add requirements file for all projects

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# `CoolPy`
-
-<img src="imgs/logo-border-black.svg" alt="coolpylogo" width="200"/>
+<p align="center">
+    <img src="imgs/logo-border-black.svg" alt="coolpylogo" width="200"/>
+</p>
 
 Welcome to the [CoolPy](https://www.tiktok.com/@pythonrepos) repository. Here, you can find the short scripts of all the scripts used in the channel. 
 
-You can find a `requirements.txt` inside each project folder with the minimum required packages needed. 
+You can find a `requirements.txt` file inside each project folder with the minimum required packages needed. 
--- video-scripts/manim-animations/manim_tiktok.py
+++ video-scripts/manim-animations/manim_tiktok.py
@@ -13,28 +13,28 @@ class TikTokVideo(Scene):
         self.play(Write(title))
         self.play(ReplacementTransform(title, equation))
 
-        # framebox1 = SurroundingRectangle(
-        #     equation[0], buff=0.1
-        # )
-        # framebox2 = SurroundingRectangle(
-        #     equation[2], buff=0.1
-        # )
-        # self.play(
-        #     Create(framebox1)
-        # )
-        # self.wait()
+        framebox1 = SurroundingRectangle(
+            equation[0], buff=0.1
+        )
+        framebox2 = SurroundingRectangle(
+            equation[2], buff=0.1
+        )
+        self.play(
+            Create(framebox1)
+        )
+        self.wait()
 
-        # ax = Axes(x_range=[-2,2], y_range=[-5,20],
-        #             x_length=6, y_legnth=8)
-        # labels = ax.get_axis_labels(
-        #     x_labels="x", ylabel="f(x)"
-        # )
-        # def fun(x):
-        #     return (2*x) ** 2 + 1
+        ax = Axes(x_range=[-2,2], y_range=[-5,20],
+                    x_length=6, y_legnth=8)
+        labels = ax.get_axis_labels(
+            x_labels="x", ylabel="f(x)"
+        )
+        def fun(x):
+            return (2*x) ** 2 + 1
         
-        # graph = ax.plot(fun, color=MAROON)
-        # g = Group(ax, labels, graph).scale(0.5).move_to(DOWN)
-        # self.play(AnimationGroup(
-        #     ReplacementTransform(framebox1, framebox2),
-        #     FadeIn(g, shift=UP * 1.5)
-        # ))
+        graph = ax.plot(fun, color=MAROON)
+        g = Group(ax, labels, graph).scale(0.5).move_to(DOWN)
+        self.play(AnimationGroup(
+            ReplacementTransform(framebox1, framebox2),
+            FadeIn(g, shift=UP * 1.5)
+        ))
